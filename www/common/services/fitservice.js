63543767f51923102183e540f9a7fe4be5cc4721
Christopher Mercadante <cmercada86@gmail.com>
2017-02-27 08:30:04 -0500

    adding basic service structure

---
+++ www/common/services/fitservice.js
@@ -0,0 +1,17 @@
+angular
+  .module('fitRewards')
+  .service('fitService',fitSevice);
+  
+  fitService.$inject =['$http','$q'];
+  
+  function fitService($http,$q){
+  
+    var service = {
+      //public functions
+      //ie: setUser:setUser,
+    };
+    
+    
+  
+    return service;
+  }
