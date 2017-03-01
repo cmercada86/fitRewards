4ee9d59989b74eaee11ce25ef20fe53efd2ab158
cmercada86 <cmercada86@gmail.com>
2017-03-01 17:11:04 +0000

    Moving to new layout

---
+++ www/app/common/services/fitservice.js
@@ -0,0 +1,15 @@
+angular.module('services', [])
+.service('FitService', function() {
+// For the purpose of this example I will store user data on ionic local storage but you should save it on a database
+  var setUser = function(user_data) {
+
+    //window.localStorage.starter_google_user = JSON.stringify(user_data);
+  };
+  var getUser = function(){
+    //return JSON.parse(window.localStorage.starter_google_user || '{}');
+  };
+  return {
+    getUser: getUser,
+    setUser: setUser
+  };
+});
